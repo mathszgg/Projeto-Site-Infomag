166f96958da8b8ca5185100728d1f39451e4bfdc
mathszgg <matheusmagrani2015@gmail.com>
2025-08-22 02:01:14 -0300

    Refine border bottom of Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import SocialNeworkBar from "./SocialNetworkBar";
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white mt-10">
+        <footer className="bg-gray-800 text-white mt-1">
             <div className="container mx-auto px-6 py-12">
                 {/* Container principal com 3 colunas */}
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
